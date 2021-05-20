7788428cf2cb510a9fbe71788aa1d6330d02460e
AnnaBiro <81937689+AnnaBiro@users.noreply.github.com>
2021-05-20 15:06:14 -0400

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -33,8 +33,21 @@ function displayTemperature(response){
   iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
-let apiKey = "0dbcd93086477c1a7e773d46c9357657";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+  let apiKey = "0dbcd93086477c1a7e773d46c9357657";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
