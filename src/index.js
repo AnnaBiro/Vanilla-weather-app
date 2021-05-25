6f6edf254ac79eb8e2aea6a44e6904dc8cd3fdca
AnnaBiro <81937689+AnnaBiro@users.noreply.github.com>
2021-05-25 13:58:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -49,6 +50,15 @@ function displayForecast() {
       forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "0dbcd93086477c1a7e773d46c9357657";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+
+}
+
 function displayTemperature(response){
  
   let temperatureElement = document.querySelector("#temperature");
@@ -69,6 +79,8 @@ function displayTemperature(response){
   dateElement.innerHTML = formatDate(response.data.dt*1000);
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt",response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city){
@@ -115,5 +127,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
 
-displayForecast();
+
 
